d56d7bb64f63d660ccc1949335c729b101fc9228
YaoTing_25 <lightchaser1125@gmail.com>
2026-09-03 16:50:43 +0800

    version update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.travelyn.travelyn"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
